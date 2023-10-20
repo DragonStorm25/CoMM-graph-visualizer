297d007bd3811f8e3fb733afcca267bef611b9b1
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-20 17:47:02 -0400

    Add imports, create Digraph

--- node_parser.py
+++ node_parser.py
@@ -1,10 +1,12 @@
-
+import networkx as nx
+import matplotlib
 
 if __name__ == "__main__":
     input_file_path = "nodes.txt"
     input_file = open(input_file_path, "r")
     input_file_text = input_file.read()
 
+    G = nx.Digraph()
     edge_list = input_file_text.split("\n")
     for edge in edge_list:
         edge_tail, edge_head = edge.replace(" ", "").split("->")
