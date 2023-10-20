f0ad756b07c12863095f756bb614b98431902854
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-20 17:42:15 -0400

    Iterate through edges and print

--- node_parser.py
+++ node_parser.py
@@ -5,4 +5,6 @@ if __name__ == "__main__":
     input_file = open(input_file_path, "r")
     input_file_text = input_file.read()
 
-    node_list = input_file_text.split("\n")
+    edge_list = input_file_text.split("\n")
+    for edge in edge_list:
+        print(edge)
