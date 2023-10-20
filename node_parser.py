203f0a35336fbd4e89ab876b90c2a46888afeb6f
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-20 17:50:06 -0400

    Fix typo

--- node_parser.py
+++ node_parser.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     input_file = open(input_file_path, "r")
     input_file_text = input_file.read()
 
-    G = nx.Digraph()
+    G = nx.DiGraph()
     edge_list = input_file_text.split("\n")
     for edge in edge_list:
         edge_tail, edge_head = edge.replace(" ", "").split("->")
