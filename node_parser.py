da7ba890c617a893ac651698fcb421377389c92c
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-20 17:41:39 -0400

    Split nodes by newline

--- node_parser.py
+++ node_parser.py
@@ -3,4 +3,6 @@
 if __name__ == "__main__":
     input_file_path = "nodes.txt"
     input_file = open(input_file_path, "r")
-    input_file_text = input_file.read();
+    input_file_text = input_file.read()
+
+    node_list = input_file_text.split("\n")
