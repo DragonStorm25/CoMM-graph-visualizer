66aae16edc7bb867330ff3dd10c30918def9fcc9
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-21 13:31:35 -0400

    Actually call init function

--- NodeParser.py
+++ NodeParser.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     input_file = open(input_file_path, "r")
     input_file_text = input_file.read()
 
-    G = GraphVisualizer()
+    G = GraphVisualizer.GraphVisualizer()
     edge_list = input_file_text.split("\n")
     for edge in edge_list:
         edge_tail, edge_head = edge.replace(" ", "").split("->")
