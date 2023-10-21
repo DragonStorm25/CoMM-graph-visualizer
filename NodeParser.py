a9f230823c2770aca92161c3b0243ef7d56a80e5
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-21 15:47:08 -0400

    Add comments to parser

--- NodeParser.py
+++ NodeParser.py
@@ -1,14 +1,20 @@
 import GraphVisualizer
 
 if __name__ == "__main__":
+    # Read and store file text
     input_file_path = "nodes.txt"
     input_file = open(input_file_path, "r")
     input_file_text = input_file.read()
 
+    # Create graph visualizer
     G = GraphVisualizer.GraphVisualizer()
+
+    # Parse input text and add all edges
     edge_list = input_file_text.split("\n")
     for edge in edge_list:
         edge_tail, edge_head = edge.replace(" ", "").split("->")
         G.addEdge(edge_tail, edge_head)
     G.addLabel("18.01", "Calculus 1 and 2")
+
+    # Visualize graph
     G.visualize()
