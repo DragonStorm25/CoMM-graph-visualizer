1c45815021a33f05173a74ee836f78ebf3f19ad7
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-21 01:02:14 -0400

    Add visualization code

--- node_parser.py
+++ node_parser.py
@@ -1,15 +1,30 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+class GraphVisualization: 
+   
+    def __init__(self): 
+        self.visual = [] 
+          
+    def addEdge(self, a, b): 
+        temp = [a, b] 
+        self.visual.append(temp) 
+
+    def visualize(self): 
+        G = nx.DiGraph() 
+        G.add_edges_from(self.visual) 
+        nx.draw_kamada_kawai(G, with_labels=True) 
+        plt.show() 
+
 if __name__ == "__main__":
     input_file_path = "nodes.txt"
     input_file = open(input_file_path, "r")
     input_file_text = input_file.read()
 
-    G = nx.DiGraph()
+    G = GraphVisualization()
     edge_list = input_file_text.split("\n")
     for edge in edge_list:
         edge_tail, edge_head = edge.replace(" ", "").split("->")
         print(f"tail: {edge_tail}, head: {edge_head}")
-        G.add_edge(edge_tail, edge_head)
-    nx.draw(G)
+        G.addEdge(edge_tail, edge_head)
+    G.visualize()
