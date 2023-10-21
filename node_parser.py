19e59b0e42ad515955530668b6b4c1a8083d6f93
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-21 02:03:06 -0400

    Add basic tooltips

--- node_parser.py
+++ node_parser.py
@@ -15,8 +15,34 @@ class GraphVisualization:
         G.add_edges_from(self.visual) 
         circle_pos = nx.circular_layout(G)
         pos = nx.arf_layout(G, pos=circle_pos, a=5)
-        nx.draw(G, pos, with_labels=True, node_size=[len(v) ** 2 * 60 for v in G.nodes()])
-        plt.show() 
+        fig,ax = plt.subplots() 
+        nodes = nx.draw_networkx_nodes(G, pos, node_size=[len(v) ** 2 * 60 for v in G.nodes()], ax=ax)
+        annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+        def update_annot(ind):
+            node = ind["ind"][0]
+            xy = pos[list(G.nodes())[node]]
+            annot.xy = xy
+            node_attr = {'node': node}
+            node_attr.update({node: list(G.nodes())[node]})
+            text = '\n'.join(f'{k}: {v}' for k, v in node_attr.items())
+            annot.set_text(text)
+
+        def hover(event):
+            vis = annot.get_visible()
+            if event.inaxes == ax:
+                cont, ind = nodes.contains(event)
+                if cont:
+                    update_annot(ind)
+                    annot.set_visible(True)
+                    fig.canvas.draw_idle()
+                else:
+                    if vis:
+                        annot.set_visible(False)
+                        fig.canvas.draw_idle()
+        fig.canvas.mpl_connect("motion_notify_event", hover)
+        plt.show()
 
 if __name__ == "__main__":
     input_file_path = "nodes.txt"
