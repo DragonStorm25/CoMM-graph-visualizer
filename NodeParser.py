e7ce6ddacf529e5a2bc827592045c29c730be19e
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-21 20:22:53 -0400

    Add comments

--- NodeParser.py
+++ NodeParser.py
@@ -4,21 +4,23 @@ if __name__ == "__main__":
     # Create graph visualizer
     G = GraphVisualizer.GraphVisualizer()
 
-    # Read and store file text
+    # Read and store node text
     node_file_path = "nodes.txt"
     node_file = open(node_file_path, "r")
     node_file_text = node_file.read()
 
-    # Parse input text and add all edges
+    # Parse node text and add all edges
     edge_list = node_file_text.split("\n")
     for edge in edge_list:
         edge_tail, edge_head = edge.replace(" ", "").split("->")
         G.addEdge(edge_tail, edge_head)
 
+    # Read and store tooltip text
     tooltip_file_path = "tooltip.txt"
     tooltip_file = open(tooltip_file_path, "r")
     tooltip_file_text = tooltip_file.read()
 
+    # Parse tooltip text and add all labels
     tooltip_list = tooltip_file_text.split("\n")
     for tooltip in tooltip_list:
         node, text = tooltip.split(" = ")
